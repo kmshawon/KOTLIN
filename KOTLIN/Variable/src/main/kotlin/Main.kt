b7c3a86e5763ab32ea20e118a439362b0209640f
MD ABDUL KHALEQUE <56734871+kmshawon@users.noreply.github.com>
2023-02-21 18:43:14 +0600

    Add files via upload

---
+++ KOTLIN/Variable/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var userName = "Khaleque"
+    userName= "Md Abdul Khaleque"
+
+    var age:Int=22
+    age=25
+
+    println("Hello $userName Your age is $age")
+
+
+}
